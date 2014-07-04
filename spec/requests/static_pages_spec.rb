81260a21504193b5d165a7e37780d68207d12d09
Ryan Maw <ryanwazhere4490@gmail.com>
2014-07-03 19:11:25 -0600

    View For Home Updated

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,30 +12,49 @@ require 'spec_helper'
 
 
 describe "Static pages" do
-	# TEST DESCRITPTION
-	# Pass: The home page does contain the phrase 'Sample App'
+
 	describe "Home page" do
+		# TEST DESCRITPTION
+		# Pass: The home page does contain the phrase 'Sample App'
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
+		# TEST DESCRIPTION
+		# Pass: <Title> must contain 'Home'
+		it "should have the right title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
 	end	
-	
-	# TEST DESCRITPTION
-	# Pass: The Help page does contain the phrase 'Help'
-	describe "Help page" do
+
+	describe "Help page" do	
+		# TEST DESCRITPTION
+		# Pass: The Help page does contain the phrase 'Help'
 		it "should have the content 'Help" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+		# TEST DESCRITPTION
+		# Pass: <title div> must contain 'Help'
+		it "should have the right title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Help")
+		end
 	end
 
-	# TEST DESCRITPTION
-	# Pass: The About page does contain the phrase 'About Us'
 	describe "About page" do
+		# TEST DESCRITPTION
+		# Pass: The About page does contain the phrase 'About Us'
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+		# TEST DESCRITPTION
+		# Pass: <title div> must contain 'About Us'
+		it "should have the right title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("About")
 		end
 	end
 end
