093dabad429a787db5fdcda2fdbc40e0e982923f
Ryan Maw <ryanwazhere4490@gmail.com>
2014-07-19 16:50:34 -0600

    Has Selector Used For Static Page Content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,32 +12,28 @@ require 'spec_helper'
 
 
 describe "Static pages" do
-
+	subject { page }
 	describe "Home page" do
 		before{ visit root_path }
-		subject { page }
-		it { should have_content('Sample App')}
+		it { should have_selector('body', text: 'Sample App')}
 		it { should have_title(full_title('')) }
 		it { should_not have_title('| Home') }
 	end	
 
 	describe "Help page" do	
 		before { visit help_path }
-		subject {page}
-		it { should have_content('Help') }
+		it { should have_selector('body', text: 'Help') }
 		it { should have_title(full_title("Help")) }
 	end
 
 	describe "About page" do
 		before { visit about_path }
-		subject { page }
-		it { should have_content('About Us') }
+		it { should have_selector('body', text: 'About Us') }
 		it { should have_title(full_title("About")) }
 	end
 	describe "Contact Page" do
 		before { visit contact_path }
-		subject { page }
-		it { should have_content('Contact') }
+		it { should have_selector('body', text: 'Contact') }
 		it { should have_title(full_title("Contact")) }
 	end
 end
