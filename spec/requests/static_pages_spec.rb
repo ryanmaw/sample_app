99024752cf4a99d34913cdf0c2b865d9ef42f52d
Ryan Maw <ryanwazhere4490@gmail.com>
2014-07-19 12:48:49 -0600

    Rspec Helper Method For Titles Added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,7 +17,7 @@ describe "Static pages" do
 		before{ visit root_path }
 		subject { page }
 		it { should have_content('Sample App')}
-		it { should have_title("Ruby on Rails Tutorial Sample App") }
+		it { should have_title(full_title('')) }
 		it { should_not have_title('| Home') }
 	end	
 
@@ -25,20 +25,20 @@ describe "Static pages" do
 		before { visit help_path }
 		subject {page}
 		it { should have_content('Help') }
-		it { should have_title("Help")}
+		it { should have_title(full_title("Help")) }
 	end
 
 	describe "About page" do
 		before { visit about_path }
 		subject { page }
 		it { should have_content('About Us') }
-		it { should have_title("About") }
+		it { should have_title(full_title("About")) }
 	end
 	describe "Contact Page" do
 		before { visit contact_path }
 		subject { page }
 		it { should have_content('Contact') }
-		it { should have_title("Contact") }
+		it { should have_title(full_title("Contact")) }
 	end
 end
 
