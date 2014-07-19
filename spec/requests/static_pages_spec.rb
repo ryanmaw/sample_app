ecdfd35f85a31eb33543e163d79e3bd735faf0fe
Ryan Maw <ryanwazhere4490@gmail.com>
2014-07-19 10:34:27 -0600

    Contact Page Test Added
    Tests should be failing.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -63,5 +63,38 @@ describe "Static pages" do
 			expect(page).to have_title("About")
 		end
 	end
+	describe "Contact Page" do
+		# TEST DESCIPTION
+		# Pass:
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title('Contact')
+		end
+	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
