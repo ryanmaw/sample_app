169f8098add17ef43a4e165de5d49ed574ffb580
Ryan Maw <ryanwazhere4490@gmail.com>
2014-07-21 00:34:27 -0600

    Name Validation and Length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	# Validate that a User has a name
-	validates :name,presence: true
+	validates :name,presence: true, length: { maximum: 50 }
 	validates :email,presence: true
 end
