fc904602f193d0f0d2eaa57e713ec3065a321302
Ryan Maw <ryanwazhere4490@gmail.com>
2014-07-31 12:28:49 -0500

    Enforce Email Uniqeness At Database Level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	# Validate that a User has a name
+	before_save { self.email = email.downcase }
 	validates :name,presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+ 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,13 +40,34 @@ describe User do
 				end
 			end
 		end
-	describe "when email format is valid" do
-		it "should be valid" do
-			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-			addresses.each do |valid_address|
-				@user.email = valid_address
-				expect(@user).to be_valid
+		describe "when email format is valid" do
+			it "should be valid" do
+				addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+				addresses.each do |valid_address|
+					@user.email = valid_address
+					expect(@user).to be_valid
+				end
 			end
 		end
-	end
+		describe "when email address is already taken" do
+			before do
+				user_with_same_email = @user.dup
+				user_with_same_email.email = @user.email.upcase
+				user_with_same_email.save
+			end
+
+			it { should_not be_valid }
+		end
 end
+
+
+
+
+
+
+
+
+
+
+
+
