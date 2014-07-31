923cf6cc47d5b2e5f6f98538576115607b896627
Ryan Maw <ryanwazhere4490@gmail.com>
2014-07-31 13:50:50 -0500

    Basic Password Tests Added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe User do
-	before { @user = User.new(name: "example", email:"example@gmail.com") }
+	before { @user = User.new(name: "example", email:"example@gmail.com",password: "foobar", password_confirmation: "foobar") }
 	subject { @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
-
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 	it { should be_valid }
 
 	describe "when name is not present" do
@@ -21,6 +23,17 @@ describe User do
 
 	describe "when name is to long" do
 		before { @user.name = 'a' * 51 }
+		it { should_not be_valid }
+	end
+	describe "when password is not present" do
+	  before do
+	    @user = User.new(name: "Example User", email: "user@example.com",
+	                     password: " ", password_confirmation: " ")
+	  end
+	  it { should_not be_valid }
+	end	
+	describe "when password doesn't match confirmation" do
+		before { @user.password_confirmation = "mismatch" }
 		it { should_not be_valid }
 	end
 
@@ -40,24 +53,23 @@ describe User do
 				end
 			end
 		end
-		describe "when email format is valid" do
-			it "should be valid" do
-				addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-				addresses.each do |valid_address|
-					@user.email = valid_address
-					expect(@user).to be_valid
-				end
+	describe "when email format is valid" do
+		it "should be valid" do
+			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+			addresses.each do |valid_address|
+				@user.email = valid_address
+				expect(@user).to be_valid
 			end
 		end
-		describe "when email address is already taken" do
-			before do
-				user_with_same_email = @user.dup
-				user_with_same_email.email = @user.email.upcase
-				user_with_same_email.save
-			end
-
-			it { should_not be_valid }
+	end
+	describe "when email address is already taken" do
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save
 		end
+		it { should_not be_valid }
+	end
 end
 
 
