ef3d81ad38245dd131ad1abab736ffaeffd263f3
Ryan Maw <ryanwazhere4490@gmail.com>
2014-08-03 15:08:23 -0500

    7.0

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 
 describe User do
 	before do
-	    @user = User.new(name: "Example User", email: "user@example.com",
-	                     password: "foobar", password_confirmation: "foobar")
+	    @user = User.new(name: "Example User", 
+	    				 email: "user@example.com",
+	                     password: "foobar", 
+	                     password_confirmation: "foobar")
     end
 
 	subject { @user }
@@ -39,55 +41,34 @@ describe User do
 		end
 	end
 
-	#### User Name Tests ####
-	describe "when name is to long" do
-		before { @user.name = 'a' * 51 }
-		it { should_not be_valid }
-	end
-	#### User Password Tests ####
-	describe "when password is not present" do
-	  before do
-	    @user = User.new(name: "Example User", email: "user@example.com",
-	                     password: " ", password_confirmation: " ")
-	  end
-	  it { should_not be_valid }
-	end	
-	describe "when password doesn't match confirmation" do
-		before { @user.password_confirmation = "mismatch" }
-		it { should_not be_valid }
-	end
-	describe "with a password that's too short" do
-	    before { @user.password = @user.password_confirmation = "a" * 5 }
-	    it { should be_invalid }
-	end
-
-
-
-	# Email Validations
-		# Do a passing test
-		# Do a failing test
-		# This will catch MOST cases
-
-		# When is not invalid and valid 
 	describe "when email format is invalid" do
 		it "should be invalid" do
-			addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-				foo@bar_baz.com foo@bar+baz.com]
+			addresses = %w[
+				user@foo,com user_at_foo.org
+				example.user@foo.
+				foo@bar_baz.com 
+				foobar@gmail..com
+				foo@bar+baz.com]
 			addresses.each do |invalid_address|
 				@user.email = invalid_address
 				expect(@user).not_to be_valid
 			end
 		end
 	end
+
 	describe "when email format is valid" do
 		it "should be valid" do
-			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+			addresses = %w[user@foo.COM 
+				A_US-ER@f.b.org
+			 	frst.lst@foo.jp 
+			 	a+b@baz.cn]
 			addresses.each do |valid_address|
 				@user.email = valid_address
 				expect(@user).to be_valid
 			end
 		end
 	end
+
 	describe "when email address is already taken" do
 		before do
 			user_with_same_email = @user.dup
@@ -96,6 +77,33 @@ describe User do
 		end
 		it { should_not be_valid }
 	end
+
+	#### User Name Tests ####
+	describe "when name is to long" do
+		before { @user.name = 'a' * 51 }
+		it { should_not be_valid }
+	end
+	#### User Password Tests ####
+	describe "when password is not present" do
+	  before do
+	    @user = User.new(name: "Example User", 
+	    				 email: "user@example.com",
+	                     password: " ",
+	                     password_confirmation: " ")
+	  end
+	  it { should_not be_valid }
+	end	
+
+	describe "when password doesn't match confirmation" do
+		before { @user.password_confirmation = "mismatch" }
+		it { should_not be_valid }
+	end
+
+	describe "with a password that's too short" do
+	    before { @user.password = @user.password_confirmation = "a" * 5 }
+	    it { should be_invalid }
+	end
+
 	describe "return value of authenticate method" do
 		before { @user.save }
 		let(:found_user) { User.find_by(email: @user.email) }
@@ -111,6 +119,8 @@ describe User do
 			specify { expect(user_for_invalid_password).to be_falsey }
 		end
 	end
+
+
 end
 
 
