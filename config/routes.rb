76240ce494580065c3f7facc326117c2bb4c2c98
Ryan Maw <ryandouglasmaw@gmail.com>
2014-09-25 16:14:26 -0600

    Sign in Invalid Case Implimented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
@@ -7,11 +7,18 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+
+  resources :users
   match 'help', to: 'static_pages#help', via: 'get'
   match 'about', to: 'static_pages#about', via: 'get'
   match 'contact', to: 'static_pages#contact', via: 'get'
   match 'signup', to: 'users#new', via: 'get'
 
+
+  resources :sessions, only:[:new, :create, :destroy]
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
