a9c09a4edb3a7303323cf4812382987b1af4dc7b
Ryan Maw <ryandouglasmaw@gmail.com>
2014-10-28 15:43:05 -0600

    Successful Logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,10 @@ class SessionsController < ApplicationController
 	  if user && user.authenticate(params[:session][:password])
 	    # Sign the user in and redirect to the user's show page.
 	    log_in user
+
+	    # Rails automatically converts this to the route for the users profile page "user_path(user)"
 	    redirect_to user
+
 	  else
 	    # Create an error message and re-render the signin form.
 	    flash.now[:danger] = 'Invalid Email / Password Combination'
