d331cd3a153abc85dc85dfebdb4ff5d8984c0364
Ryan Maw <ryandouglasmaw@gmail.com>
2014-10-28 16:30:12 -0600

    Code for finding current user in session added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,15 @@ class SessionsController < ApplicationController
 	  end
 	end
 
+	# 8.2.2 in notes.txt
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
 	def destroy
 	end
 end
