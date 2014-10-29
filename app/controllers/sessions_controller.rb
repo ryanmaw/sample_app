dc941743e8d71008459a50d1bc9787a15d27ccf7
Ryan Maw <ryandouglasmaw@gmail.com>
2014-10-28 20:51:27 -0600

    8.2.4
    Moved some things from the controller to the session helper.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,18 +18,7 @@ class SessionsController < ApplicationController
 	  end
 	end
 
-	# 8.2.2 in notes.txt
-	def log_in(user)
-		session[:user_id] = user.id
-	end
 
-	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
-	end
-
-	def logged_in?
-		!current_user.nil?
-	end
 
 
 	def destroy
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,4 +18,16 @@ module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
+
+	# 8.2.2 in notes.txt
+
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	def logged_in?
+		!current_user.nil?
+	end
+
+
 end
