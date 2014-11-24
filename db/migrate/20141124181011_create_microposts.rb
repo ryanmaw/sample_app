acb3023a13c9b8b12dd27a83591555a70e2159c4
ryanmaw <ryan@ryanmaw.com>
2014-11-24 11:16:54 -0700

    Multiple Key Index added to Microposts Migration

--- db/migrate/20141124181011_create_microposts.rb
+++ db/migrate/20141124181011_create_microposts.rb
@@ -6,5 +6,10 @@ class CreateMicroposts < ActiveRecord::Migration
 
       t.timestamps
     end
+    # We expect to retrieve all the microposts associated with a given user id in reverse order of creation.
+    # This adds an index
+    # Rails then creates a multiple key index, which means that Active Record uses both keys at the same time.
+    
+    add_index :microposts, [ :user_id, :created_at ]
   end
 end
